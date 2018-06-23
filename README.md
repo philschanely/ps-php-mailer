abf6b4eb5c9456c20fc4bb223ea7d5dbfbc8b26b
Phil Schanely <philschanely@gmail.com>
2018-06-23 18:10:54 -0400

    Improve instructions and some script

--- README.md
+++ README.md
@@ -4,4 +4,50 @@ The script contained here allows you to set up a very simple HTML email sender u
 
 ## Setup
 
-Download the provided files and upload into the desired directory on a PHP server so that the as `http://yourdomain.com/mailer`
+Download the provided files and place or upload them into the desired directory on a PHP server. Ensure that the location is a url that can be called something like `http://yourdomain.com/mailer`.
+
+### Authentication code setup
+
+If you want to use an authentication code to increase the security of the script, ensure that `$require_auth` is set to `true`:
+
+```php
+// Indicate whether or not an authorization code is required
+$require_auth = false;
+```
+
+If you decide to enable this then you must also create a file in the same directory called `env.php`. Copy and paste the contents of `env.sample.php` into your new `env.php` file. Then update the value stored in `AUTH` to include your own unique authorization code such as an [MD5 hash](http://www.miraclesalad.com/webtools/md5.php).
+
+## API
+
+This script must be called using a POST request that passes the following parameters:
+
+| Parameter | Purpose |
+|:--|:--|
+| `to` | The email address to which the message should be sent. |
+| `from` | The email address from which the message will appear to have been sent. |
+| `subject` | The subject line for the email message. |
+| `message` | The HTML formatted message to be included in the email. |
+| `auth` | *(Optionally)* The matching authorization code set in `env.php` as described above. |
+
+## Example Client Code
+
+This script is intended to be callable from a client-side application such as one written in JavaScript. Using the [`axios`](http://localhost) library a well-formed request can be sent and handled like this:
+
+```js
+let axios = require('axios');
+axios.post('http://yourdomain.com/mailer', {
+  auth: 'YOUR_AUTH_CODE',
+  to: 'myeamil@example.com',
+  from: 'youremail@elsexample.com',
+  subject: 'Test message',
+  message: '<p>Hello!</p><p>We are glad you have decided to try out Mailer. We hope it works <strong>really</strong> well for you!</p>'
+}).then(data => {
+  if (data.response === 'SUCCESS') {
+    console.log('Message was successfully sent to the mailer api.');
+  } else {
+    console.error('An error was returned by the mailer api:' . data.response);
+  }
+}).catch(err) {
+  console.error('An error occurred with the mailer api request', err.message);
+};
+```
--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 
+// Allow calls from elsewhere :)
+header("Access-Control-Allow-Origin: *");
 
-# ini_set('display_errors', 1);
+// Toggle displaying errors while testing implementation
+ini_set('display_errors', 1);
 
-require_once('env.php');
+// Get the AUTH constant
+include('env.php');
 
+// Indicate whether or not an authorization code is required
+$require_auth = false;
+
+// Get the values sent via POST request.
 // $params = $_POST;
 $params = array(
   'from' => 'schanelyphotography@gmail.com',
@@ -12,17 +20,41 @@ $params = array(
   'subject' => 'Hello!',
   'message' => '<p>Hey dude.</p><p>sincerely, your friend</p>'
 );
+
+// List of required parameters for validation and feedback
 $requred_params = array('to', 'from', 'subject', 'message');
+
+// List of missing params to use in validation and feedback
 $missing_params = array();
 
+// Response object to modify in this script and deliver feedback to client
+$response = (object) array(
+  'response' => 'ERROR',
+  'params' => $params,
+  'headers' => null,
+  'message' => ''
+);
 
+
+// Validate request and send message
+
+// 1.
 // Make sure params exist
 if (empty($params)) {
-  echo 'ERROR: No params provided.';
+  $response->message = 'ERROR: No params provided.';
+  echo json_encode($response);
   die;
 }
 
+// 2.
+// Ensure requrest has come with correct AUTH code if one is required
+if ($require_auth && AUTH != $params['auth']) {
+  $response->message = 'ERROR: Invalid authorization code';
+  echo json_encode($response);
+  die;
+}
 
+// 3.
 // Make sure all required params are present
 foreach ($requred_params as $key) {
   if (!array_key_exists($key, $params)) {
@@ -30,13 +62,15 @@ foreach ($requred_params as $key) {
   }
 }
 if (!empty($missing_params)) {
-  echo 'ERROR: Missing required params: ' . implode(', ', $missing_params);
+  $response->message = 'ERROR: Missing required params: ' . implode(', ', $missing_params);
+  echo json_encode($response);
   die;
 }
 
+// Change params to an object
 $params = (object) $params;
 
-
+// 4.
 // Compose the message
 $message = "
 <html>
@@ -49,22 +83,25 @@ $message = "
 </html>
 ";
 
-
 // Set up headers
 $headers[] = 'MIME-Version: 1.0';
 $headers[] = 'Content-type: text/html; charset=iso-8859-1';
-$headers[] = 'To: ' . $params->to;
-$headers[] = 'From: ' . $params->from;
+$headers[] = "To: Recipient <{$params->to}>";
+$headers[] = "From: Sender <{$params->from}>";
 
-
+// 5.
 // Mail it
-mail($params->to, $params->subject, $message, implode("\r\n", $headers));
+$sent_mail = mail($params->to, $params->subject, $message, implode("\r\n", $headers));
+if (!$sent_mail) {
+  $response->message = 'ERROR: Mail did not send properly. ';
+  echo json_encode($response);
+  die;
+}
 
-
-// Show a response to the API caller
-$response = (object) array(
-  'params_provided' => $params,
-  'headers' => $headers,
-  'message' => $message
-);
+// Show a success response to the client
+$response->response = 'SUCCESS';
+$response->headers = $headers;
+$response->message = $message;
+echo '<pre>';
 echo json_encode($response);
+echo '</pre>';
